57a912ca3b1a8786ddbdf3f8a90d5ef3b8226d4c
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-07 18:13:27 +0530

    beginnings of java code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,62 @@
+$(document).ready(function() {
+    console.log("working");
 
-$(document).ready(fuction(){ 
-console.log("working");
+    $(".navigateForward").click(function() {
+        console.log("march forward!");
+        $("question_item").text("qcycle()");
+        $(".question_item").text("This actress starred opposite Terrence Howard in which 2006 Oscar-winning Best Picture?");
 
+    });
 
 })
+
+
+// Need to create a loop that goes through the array items, filling the fields. 
+
+// Need to create a class of events that plug into existing containers on the page
+
+var question = {
+    two: $(".question2").text(),
+    three: $(".question3").text(),
+    four: $(".question4").text(),
+    five: $(".question5").text(),
+    qcycle: function() {
+        for (var prop in question) {
+            return question(prop);
+        }
+    }
+};
+
+var contents3 = {
+
+    image: $(".hayek"),
+    answers: $("#answers2").text(),
+    complete: function() {
+        return this.image + this.question + this.answers;
+    }
+};
+
+var contents4 = {
+
+    image: $(".oyelowo"),
+    answers: $("#answers2").text(),
+    complete: function() {
+        return this.image + this.question + this.answers;
+    }
+
+};
+
+var contents5 = {
+
+    image: $(".henson"),
+    answers: $("#answers2").text(),
+    complete: function() {
+        return this.image + this.question + this.answers;
+    }
+};
+
+// var content_container = [contents2, contents3, contents4, contents5];
+
+function filler() {
+
+}
