f638d8d1dcca1490e0125dcb9453523056e22f5e
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-08 14:12:43 +0530

    figuring out java code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,86 @@
+var questions = $(".question_item > p").toArray();
+var images = $(".container_CharacterPhoto > div").toArray();
+var answers = $(".options > ul").toArray();
+console.log(questions.length);
+
+function fill (){
+    for(i=0; i<=questions.length; i=i+1){
+    	return (questions[i]);
+    	console.log((questions[i]));
+    }
+
+
+    // question: $(".question_item"),
+    // image: $(".container_CharacterPhoto"),
+    // answer: $(".options"),
+};
+
+// var freeman = Object.create(content);
+// freeman.question = $(".question_item").text($(questions[0]).text());
+
+// freeman = {
+//     question $(".question_item").children(".question1").text(),
+//     image = $(".container_CharacterPhoto").children(".freeman"),
+//     answers = $(".options").children(".choices1").text(),
+// }
+
+// freeman ={
+// 	// question: $(".question_item").children(".question1").text(),
+// 	image:$(".container_CharacterPhoto").children(".freeman"),
+// 	answers:$(".options").children(".choices1").text(),
+// 	// test:"Hey",
+// 	        fill:function(){
+// 				return ("x");
+// 		}	
+// }
+
+// var newton = Object.create(freeman);
+
+// newton
+
+// newton = {
+//     question: $(".question_item").children(".question2").text(),
+//     image: $(".container_CharacterPhoto").children(".newton"),
+//     answers: $(".options").children(".choices2").text(),
+// }
+
+// console.log(freeman.question);
+
+// console.log(content.question);
+
 $(document).ready(function() {
     console.log("working");
 
     $(".navigateForward").click(function() {
+        $(".question_item").children(fill()).show();
+        console.log(fill());
         console.log("march forward!");
-        $("question_item").text(getQuestions());
-    });
+    })
 
-})
+});
+
+// Create an object with the 3 attributes. With each click, create a new object w/ different values.
+// var contents2 = {
+//     question: $(".question_item").text($(".question1").text()),
+//     image: $(".hayek"),
+//     answers: $("#answers2").text(),
+//     complete: function() {
+//         return this.question;
+//         console.log("this.question");
+//     }
+// };
+
+// var contents3 = Object.create(contents2);
+
+// contents3 = {
+//     question: $(".question_item").text($(".question2").text()),
+//     image: $(".hayek"),
+//     answers: $("#answers2").text(),
+
+// }
+
+
+// })
 
 
 // Need to create a loop that goes through the array items, filling the fields. 
@@ -14,15 +88,18 @@ $(document).ready(function() {
 // Need to create a class of events that plug into existing containers on the page
 
 
-var questions = $(".question_item > p").toArray();
+// function getQuestions(){
+// 	questions;
+// 	for (i=0; i<=questions.valueOf();i++){
+// 		return questions[i];
+// 		console.log("questions.valueOf()");
+// 	}
+// }
 
-function getQuestions(){
-	questions;
-	for (i=0; i<=questions.valueOf();i++){
-		return questions[i];
-		console.log("questions[i]")
-	}
-}
+
+
+
+
 
 // var question = new Array(); 
 //     two: $(".question2").text(),
@@ -37,36 +114,23 @@ function getQuestions(){
 //     }
 // };
 
-var contents3 = {
 
-    image: $(".hayek"),
-    answers: $("#answers2").text(),
-    complete: function() {
-        return this.image + this.question + this.answers;
-    }
-};
 
-var contents4 = {
+// var contents4 = {
 
-    image: $(".oyelowo"),
-    answers: $("#answers2").text(),
-    complete: function() {
-        return this.image + this.question + this.answers;
-    }
+//     image: $(".oyelowo"),
+//     answers: $("#answers2").text(),
+//     complete: function() {
+//         return this.image + this.question + this.answers;
+//     }
 
-};
+// };
 
-var contents5 = {
+// var contents5 = {
 
-    image: $(".henson"),
-    answers: $("#answers2").text(),
-    complete: function() {
-        return this.image + this.question + this.answers;
-    }
-};
-
-// var content_container = [contents2, contents3, contents4, contents5];
-
-function filler() {
-
-}
+//     image: $(".henson"),
+//     answers: $("#answers2").text(),
+//     complete: function() {
+//         return this.image + this.question + this.answers;
+//     }
+// };
