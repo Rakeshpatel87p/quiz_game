3ad5031a986927a34ee18487403a64d0cfd03dff
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-11 05:52:11 +0530

    resolving java function error

--- js/script.js
+++ js/script.js
@@ -1,145 +1,60 @@
+// Push all of my question elements into an array for easy callback
 var questions = $(".question_item > p").toArray();
 var images = $(".container_CharacterPhoto > div").toArray();
 var answers = $(".options > ul").toArray();
-var i = 0 
+var i = 0;
 
-function plus(){
-	i=0;
-	return i=i+1;
+var content = {
+    question: $(questions[i]).show(),
+    image: $(images[i]).show(),
+    answer: $(answers[i]).show(),
+    fill: function() {
+        return 
+        	$(questions[i]).show().siblings().hide(),
+            $(images[i]).show().siblings().hide(),
+            $(answers[i]).show().siblings().hide(),
+            console.log(i);
+    }
 };
+
+// .siblings().hide(),
 
 console.log(questions.length);
 
-
-function fill() {
-    for (i = 0; i <= questions.length; i = i + 1) {
-        return (questions[i]);
-        console.log((questions[i]));
-    }
-
-
-    // question: $(".question_item"),
-    // image: $(".container_CharacterPhoto"),
-    // answer: $(".options"),
-};
-
-// var freeman = Object.create(content);
-// freeman.question = $(".question_item").text($(questions[0]).text());
-
-// freeman = {
-//     question $(".question_item").children(".question1").text(),
-//     image = $(".container_CharacterPhoto").children(".freeman"),
-//     answers = $(".options").children(".choices1").text(),
-// }
-
-// freeman ={
-// 	// question: $(".question_item").children(".question1").text(),
-// 	image:$(".container_CharacterPhoto").children(".freeman"),
-// 	answers:$(".options").children(".choices1").text(),
-// 	// test:"Hey",
-// 	        fill:function(){
-// 				return ("x");
-// 		}	
-// }
-
-// var newton = Object.create(freeman);
-
-// newton
-
-// newton = {
-//     question: $(".question_item").children(".question2").text(),
-//     image: $(".container_CharacterPhoto").children(".newton"),
-//     answers: $(".options").children(".choices2").text(),
-// }
-
-// console.log(freeman.question);
-
-// console.log(content.question);
+// Log selections and then output score with correct/incorrent answers
 
 $(document).ready(function() {
     console.log("working");
     $(".navigateForward").click(function() {
-            plus();
+        // if (i>=0 && i < 4) {
+            i += 1;
+        	content.fill();
+        	console.log(content.fill());
+	    // };
+        console.log(i);
+        console.log("march forward!");
+    });
+
+    $(".navigateBack").click(function() {
+
+        if (i=0){
+        	content.fill();
+        	console.log(i)
+        } 
+        else if (i > 0 && i < 4) {
+            // i -= 1
+            console.log("moving back!");
             console.log(i);
-            $(".question_item").children(fill()).show();
-            console.log(fill());
-            console.log("march forward!");
-    })
+            backObjectShow();
+        }
+    });
 
 });
 
-// Create an object with the 3 attributes. With each click, create a new object w/ different values.
-// var contents2 = {
-//     question: $(".question_item").text($(".question1").text()),
-//     image: $(".hayek"),
-//     answers: $("#answers2").text(),
-//     complete: function() {
-//         return this.question;
-//         console.log("this.question");
-//     }
-// };
-
-// var contents3 = Object.create(contents2);
-
-// contents3 = {
-//     question: $(".question_item").text($(".question2").text()),
-//     image: $(".hayek"),
-//     answers: $("#answers2").text(),
-
-// }
-
-
-// })
-
-
-// Need to create a loop that goes through the array items, filling the fields. 
-
-// Need to create a class of events that plug into existing containers on the page
-
-
-// function getQuestions(){
-// 	questions;
-// 	for (i=0; i<=questions.valueOf();i++){
-// 		return questions[i];
-// 		console.log("questions.valueOf()");
-// 	}
-// }
-
-
-
-
-
-
-// var question = new Array(); 
-//     two: $(".question2").text(),
-//     three: $(".question3").text(),
-//     four: $(".question4").text(),
-//     five: $(".question5").text(),
-//     qcycle: function() {
-//         for (var prop in question) {
-//             console.log(question[prop]);
-//             // console.log(question.size());
-//         }
-//     }
-// };
-
-
-
-// var contents4 = {
-
-//     image: $(".oyelowo"),
-//     answers: $("#answers2").text(),
-//     complete: function() {
-//         return this.image + this.question + this.answers;
-//     }
-
-// };
-
-// var contents5 = {
-
-//     image: $(".henson"),
-//     answers: $("#answers2").text(),
-//     complete: function() {
-//         return this.image + this.question + this.answers;
-//     }
-// };
+function backObjectShow() {
+        $(questions[i]).prev().show().siblings().hide();
+        $(images[i]).prev().show().siblings().hide();
+        $(answers[i]).prev().show().siblings().hide();
+        // $(images[i - 1]).show().siblings().hide();
+        // $(answers[i - 1]).show().siblings().hide();
+}
