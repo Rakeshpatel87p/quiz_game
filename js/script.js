06cd84360ea0fb43658dffbb37a70222d04480f0
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-11 10:22:14 +0530

    finished scrolling throughquestions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Push all of my question elements into an array for easy callback
 var questions = $(".question_item > p").toArray();
 var images = $(".container_CharacterPhoto > div").toArray();
 var answers = $(".options > ul").toArray();
@@ -9,52 +8,45 @@ var content = {
     image: $(images[i]).show(),
     answer: $(answers[i]).show(),
     fill: function() {
-        return 
-        	$(questions[i]).show().siblings().hide(),
+        return $(questions[i]).show().siblings().hide(),
             $(images[i]).show().siblings().hide(),
             $(answers[i]).show().siblings().hide(),
             console.log(i);
     }
 };
 
-// .siblings().hide(),
-
-console.log(questions.length);
-
-// Log selections and then output score with correct/incorrent answers
+//output score with correct/incorrent answers
 
 $(document).ready(function() {
     console.log("working");
     $(".navigateForward").click(function() {
-        // if (i>=0 && i < 4) {
+        if (i >= 0 && i < 4) {
             i += 1;
-        	content.fill();
-        	console.log(content.fill());
-	    // };
+            content.fill();
+            console.log(content.fill());
+        };
         console.log(i);
         console.log("march forward!");
     });
 
     $(".navigateBack").click(function() {
+        backObjectShow();
+    });
 
-        if (i=0){
-        	content.fill();
-        	console.log(i)
-        } 
-        else if (i > 0 && i < 4) {
-            // i -= 1
-            console.log("moving back!");
-            console.log(i);
-            backObjectShow();
-        }
+    $(".options li").click(function() {
+        console.log("working");
+        $(this).closest("li").addClass('highlight').siblings().removeClass('highlight');
+        return(false);
     });
 
 });
 
 function backObjectShow() {
-        $(questions[i]).prev().show().siblings().hide();
-        $(images[i]).prev().show().siblings().hide();
-        $(answers[i]).prev().show().siblings().hide();
-        // $(images[i - 1]).show().siblings().hide();
-        // $(answers[i - 1]).show().siblings().hide();
+    $(questions[i]).prev().show().siblings().hide();
+    $(images[i]).prev().show().siblings().hide();
+    $(answers[i]).prev().show().siblings().hide();
+    if (i >= 0) {
+        return i -= 1;
+        console.log(i);
+    }
 }
