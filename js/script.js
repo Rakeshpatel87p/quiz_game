4f4eefdc189b692c2f9ccdae15770fe0e4c1ab41
Rakesh Patel <emailme@rakeshrpatel.com>
2016-04-11 17:15:57 +0530

    finished

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var questions = $(".question_item > p").toArray();
 var images = $(".container_CharacterPhoto > div").toArray();
 var answers = $(".options > ul").toArray();
 var i = 0;
+var x = 0;
 
 var content = {
     question: $(questions[i]).show(),
@@ -15,38 +16,43 @@ var content = {
     }
 };
 
-//output score with correct/incorrent answers
-
 $(document).ready(function() {
     console.log("working");
     $(".navigateForward").click(function() {
-        if (i >= 0 && i < 4) {
+        if (i < 4) {
             i += 1;
             content.fill();
-            console.log(content.fill());
+        } else if (i = 4) {
+            $(".end").show().text("You're Done! You got " + x + " out of 5 questions right.").siblings().hide();
+            $(".playAgain").show();
+            $(images[i]).hide();
+            $(answers[i]).hide();
         };
+        
+        $(".navigateForward").hide();
         console.log(i);
         console.log("march forward!");
     });
 
-    $(".navigateBack").click(function() {
-        backObjectShow();
-    });
-
     $(".options li").click(function() {
         console.log("working");
-        $(this).closest("li").addClass('highlight').siblings().removeClass('highlight');
-        return(false);
+        $(this).closest("li").addClass("highlight").siblings().hide();
+        if ($(this).is(".correct")) {
+            x += 1
+        };
+        $(".navigateForward").show();
+        return (false);
+        
+    });
+
+    $(".playAgain").click(function() {
+        i = 0;
+        x=0;
+        console.log(i);
+        content.fill();
+        $(".options li").closest("li").removeClass("highlight").siblings().show()
+
+
     });
 
 });
-
-function backObjectShow() {
-    $(questions[i]).prev().show().siblings().hide();
-    $(images[i]).prev().show().siblings().hide();
-    $(answers[i]).prev().show().siblings().hide();
-    if (i >= 0) {
-        return i -= 1;
-        console.log(i);
-    }
-}
