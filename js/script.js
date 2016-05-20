596ab676a0b9a51e443e3dcf3e0cf6cfa0e6b768
Rakesh Patel <emailme@rakeshrpatel.com>
2016-05-20 17:14:45 -0400

    responsive additions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+// holds all questions
 var questions = $(".question_item > p").toArray();
+// holds all question-images
 var images = $(".container_CharacterPhoto > div").toArray();
+// holds all quesiton-answer choices
 var answers = $(".options > ul").toArray();
+// keeps track of which array value to pull based on clicks to forward arrow
 var i = 0;
-var x = 0;
+// keeps track of how many correct answers user gets - at end, outputed to view.
+var correctAnswers = 0;
 
+// elements that will be outputted to the user based on var i
 var content = {
     question: $(questions[i]).show(),
     image: $(images[i]).show(),
@@ -11,34 +17,32 @@ var content = {
     fill: function() {
         return $(questions[i]).show().siblings().hide(),
             $(images[i]).show().siblings().hide(),
-            $(answers[i]).show().siblings().hide(),
-            console.log(i);
+            $(answers[i]).show().siblings().hide()
     }
 };
 
 $(document).ready(function() {
-    console.log("working");
     $(".navigateForward").click(function() {
         if (i < 4) {
             i++;
             content.fill();
         } else if (i == 4) {
-            $(".end").show().text("You're Done! You got " + x + " out of 5 questions right.").siblings().hide();
+            $(".end").show().text("You're Done! You got " + correctAnswers + " out of 5 questions right.").siblings().hide();
             $(".playAgain").show();
             $(images[i]).hide();
             $(answers[i]).hide();
         };
-        
+        // Hide navigate button until user selects an answer per rules of assignment.
         $(".navigateForward").hide();
-        console.log(i);
-        console.log("march forward!");
+
     });
 
     $(".options li").click(function() {
-        console.log("working");
+        // Highlights selected answer of user. 
         $(this).closest("li").addClass("highlight").siblings().hide();
+        // Adds up the number of correct answers of the user.
         if ($(this).is(".correct")) {
-            x++
+            correctAnswers++
         };
         $(".navigateForward").show();
         return false;
@@ -47,15 +51,10 @@ $(document).ready(function() {
 
     $(".playAgain").click(function() {
         i = 0;
-        x=0;
-        console.log(i);
+        correctAnswers=0;
         content.fill();
         $(".options li").closest("li").removeClass("highlight").siblings().show()
-
 
     });
 
 });
-
-// Give comments to those reading the code
-// Better descriptive variables
